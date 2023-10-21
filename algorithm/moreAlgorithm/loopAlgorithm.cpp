bc6ace5adf76ead5d4270fdbf5c46da4fb398354
mevlutkamali <kamalimevlut424@gmail.com>
2023-10-21 12:49:24 +0300

    algorithm

---
+++ algorithm/moreAlgorithm/loopAlgorithm.cpp
@@ -0,0 +1,16 @@
+
+/*
+    Bihter Ziyagil once said:
+        'Tabi Siz Anneleri Tarafýndan Size Emanet Edilen Çocuklara Her Bakýmdan Yetersiz Gördüðünüz Bir Kadýnýn Annelik Etmesine Þiddetle Karþýsýnýz Ama.'
+
+    Cümlesindeki "anne" kelimelerini sayacak bir döngü ve kelime sayýsýný söyleyecek bir fonksiyon yazýn .
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	return 0;
+	
+}
+
